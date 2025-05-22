a489457a9d64f5827c10c0ec8500941aca139384
ahmed <ahmedadel.2003816@gmail.com>
2025-05-22 14:54:18 +0300

    Initial Commit

---
+++ AstraTech-SwiftUi/AstraTech_SwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  AstraTech_SwiftUiApp.swift
+//  AstraTech-SwiftUi
+//
+//  Created by MacBook Pro on 22/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct AstraTech_SwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
